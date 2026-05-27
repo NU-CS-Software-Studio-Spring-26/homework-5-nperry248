26f7c0f6fb19f51794a54d8853df0f842a3f5929
Nick Perry <nperry248@gmail.com>
2026-05-26 23:01:34 -0500

    Add toggle_priority action that flips high_priority

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :set_todo, only: %i[ show edit update destroy toggle_priority ]
 
   # GET /todos or /todos.json
   def index
@@ -61,6 +61,14 @@ class TodosController < ApplicationController
     respond_to do |format|
       format.html { render :hello }
       format.json { render json: "hello world!" }
+    end
+  end
+
+  def toggle_priority
+    @todo.update!(high_priority: !@todo.high_priority)
+
+    respond_to do |format|
+      format.turbo_stream
     end
   end
 
