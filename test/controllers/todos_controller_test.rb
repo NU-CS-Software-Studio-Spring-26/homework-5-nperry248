26f7322cc557e958d0ab2b40a38bff369c1853db
Nick Perry <nperry248@gmail.com>
2026-05-26 23:05:08 -0500

    Wire priority toggle UI and turbo stream response

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -56,4 +56,14 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to todos_url
   end
+
+  test "toggle_priority responds with turbo stream" do
+    assert_not @todo.high_priority
+
+    patch toggle_priority_todo_path(@todo), headers: { "Accept" => "text/vnd.turbo-stream.html" }
+
+    assert_equal "text/vnd.turbo-stream.html", response.media_type
+    assert_response :success
+    assert @todo.reload.high_priority
+  end
 end
